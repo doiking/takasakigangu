e627e0aeeca0fbd004e737b757ae3661562fd680
doiking <doi@8hammer.com>
2016-10-03 10:36:28 +0900

    スマホテンプレ無効化

--- data/class_extends/SC_Display_Ex.php
+++ data/class_extends/SC_Display_Ex.php
@@ -4,4 +4,9 @@ require_once CLASS_REALDIR . 'SC_Display.php';
 
 class SC_Display_Ex extends SC_Display
 {
+  // 追記部分
+  public static function detectDevice($reset = FALSE) {
+    return DEVICE_TYPE_PC;
+  }
+  // 追記ここまで
 }
